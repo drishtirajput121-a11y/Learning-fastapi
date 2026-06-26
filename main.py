fb6aea7c1d116d60831efaac86470f6944f3c3e1
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-06-26 15:35:59 +0530

    use post put and delete http method

--- main.py
+++ main.py
@@ -19,4 +19,29 @@ def all_product():
     return products
 @app.get("/product/{id}")
 def get_product(id: int):
-    return products[id-1]
+    for product in products:
+        if product.id == id:
+            return product
+        return {"message": "Product not found"}
+    
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+@app.put("/product")
+def update_product(id:int,product:Product):
+    for i in range(len(products)):
+        if products[i].id==id :
+            products[i]=product
+            return "product added Successfully"
+        
+    return "No Product found"
+
+@app.delete("/product")
+def delete_product(id:int):
+    for i in range(len(products)):
+        if products[i].id==id:
+            del products[i]
+            return "deleted Successfully"
+    return "No product found"
